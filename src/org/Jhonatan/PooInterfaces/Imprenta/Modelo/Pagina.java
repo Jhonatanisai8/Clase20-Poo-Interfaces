97bd7b67cd112583369141ea16d63d0f79c7bb95
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-07-14 11:41:14 -0500

    class Pagina extends Hoja

---
+++ src/org/Jhonatan/PooInterfaces/Imprenta/Modelo/Pagina.java
@@ -0,0 +1,14 @@
+package org.Jhonatan.PooInterfaces.Imprenta.Modelo;
+
+public class Pagina extends Hoja {
+
+    public Pagina(String contenido) {
+        super(contenido);
+    }
+
+    @Override
+    public String imprimir() {
+        return null;
+    }
+
+}
