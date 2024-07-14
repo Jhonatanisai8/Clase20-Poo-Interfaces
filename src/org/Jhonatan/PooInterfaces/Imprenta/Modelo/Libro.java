8a09e2cc96874baf3c958afd8783ed5b2250b05c
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-07-14 11:42:27 -0500

    class Libro : atributos

---
+++ src/org/Jhonatan/PooInterfaces/Imprenta/Modelo/Libro.java
@@ -0,0 +1,10 @@
+package org.Jhonatan.PooInterfaces.Imprenta.Modelo;
+
+import java.util.List;
+
+public class Libro {
+    List<Hoja> paginas;
+    private String autor;
+    private String titulo;
+
+}
