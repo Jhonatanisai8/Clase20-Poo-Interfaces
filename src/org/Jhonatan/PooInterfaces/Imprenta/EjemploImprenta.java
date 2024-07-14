910221a7b1e6ffdb7503dfc6a27c840437e534e4
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-07-14 10:18:45 -0500

    class EjemploImprenta: clase main creando el metodo para imprimir

--- src/org/Jhonatan/PooInterfaces/Imprenta/EjemploImprenta.java
+++ src/org/Jhonatan/PooInterfaces/Imprenta/EjemploImprenta.java
@@ -1,7 +1,14 @@
 package org.Jhonatan.PooInterfaces.Imprenta;
 
+import org.Jhonatan.PooInterfaces.Imprenta.Modelo.Hoja;
+
 public class EjemploImprenta {
     public static void main(String[] args) {
-        System.out.println("Hola");
+        System.out.println("Ejemplo De Implementacion de Interfaces");
+    }
+
+
+    public static void imprimir(Hoja imprimible) {
+        System.out.println(imprimible.imprimir());
     }
 }
