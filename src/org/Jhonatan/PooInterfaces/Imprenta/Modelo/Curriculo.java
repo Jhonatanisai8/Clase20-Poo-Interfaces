2805224f9a92da334b7aaefca64da6e4a5fbc1f9
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-07-14 10:16:33 -0500

    class Curriculo extends Hoja: dando vida al método imprimir

--- src/org/Jhonatan/PooInterfaces/Imprenta/Modelo/Curriculo.java
+++ src/org/Jhonatan/PooInterfaces/Imprenta/Modelo/Curriculo.java
@@ -7,23 +7,32 @@ public class Curriculo extends Hoja {
 
     private String persona;
     private String carrera;
-    private List<String> experiencia;
+    private List<String> experiencias;
 
     public Curriculo(String persona, String carrera, String contenido) {
         super(contenido);
         this.persona = persona;
         this.carrera = carrera;
-        this.experiencia = new ArrayList<>();
+        this.experiencias = new ArrayList<>();
     }
 
     public Curriculo addExperecia(String exp) {
-        this.experiencia.add(exp);
+        this.experiencias.add(exp);
         return this;
     }
 
     @Override
     public String imprimir() {
-        return null;
+        StringBuilder sb = new StringBuilder("Nombre: ");
+        sb.append(persona).append("\n")
+                .append("Resumen: ").append(this.contenido)
+                .append("\n").append("Profesion: ")
+                .append(this.carrera).append("\n")
+                .append("Experiencias: \n");
+        for (String ex : this.experiencias) {
+            sb.append("_").append(ex).append("\n");
+        }
+        return sb.toString();
     }
 
 }
