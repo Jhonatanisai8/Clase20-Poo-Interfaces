98438e0cb9a294d02bbb82496ec41d26a8bce9df
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-07-14 10:03:28 -0500

    clase Informe extends Hoja: atributos y constructor

---
+++ src/org/Jhonatan/PooInterfaces/Imprenta/Modelo/Informe.java
@@ -0,0 +1,19 @@
+package org.Jhonatan.PooInterfaces.Imprenta.Modelo;
+
+public class Informe extends Hoja {
+
+    private String autor;
+    private String revisor;
+
+    public Informe(String autor,String revisor,String contenido) {
+        super(contenido);
+        this.autor = autor;
+        this.revisor = revisor;
+    }
+
+    @Override
+    public String imprimir() {
+        return null;
+    }
+
+}
