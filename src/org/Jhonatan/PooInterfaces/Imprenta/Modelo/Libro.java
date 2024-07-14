b58b915b79606a3f789b82eb42d4896df00b8649
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-07-14 11:45:05 -0500

    class Libro: constructor y método addPagina

--- src/org/Jhonatan/PooInterfaces/Imprenta/Modelo/Libro.java
+++ src/org/Jhonatan/PooInterfaces/Imprenta/Modelo/Libro.java
@@ -6,5 +6,16 @@ public class Libro {
     List<Hoja> paginas;
     private String autor;
     private String titulo;
+    private Genero genero;
 
+    public Libro(String autor, String titulo, Genero genero) {
+        this.autor = autor;
+        this.titulo = titulo;
+        this.genero = genero;
+    }
+
+    public Libro addPagina(Hoja hoja) {
+        this.paginas.add(hoja);
+        return this;
+    }
 }
