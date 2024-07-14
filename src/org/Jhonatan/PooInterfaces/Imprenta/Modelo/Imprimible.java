918109d2199f44d71a14ca4c4287450604d9d72f
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-07-14 16:06:06 -0500

    interface Imprimible: añadiendo método imprimible

--- src/org/Jhonatan/PooInterfaces/Imprenta/Modelo/Imprimible.java
+++ src/org/Jhonatan/PooInterfaces/Imprenta/Modelo/Imprimible.java
@@ -2,9 +2,14 @@ package org.Jhonatan.PooInterfaces.Imprenta.Modelo;
 
 public interface Imprimible {
     // public String imprimir();
-    default String imprimir(){
+    default String imprimir() {
         return "Impriendo un valor por defecto";
     }
+
     // se puede dejar asi
     // String imprimir();
+    static void imprimir(Imprimible imprimible) {
+        System.out.println(imprimible.imprimir() + "\n");
+    }
+
 }
