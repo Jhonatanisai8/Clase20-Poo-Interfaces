87e6ba84ab70d2692c5b4ac29e5997a6d1d7220a
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-07-14 12:02:27 -0500

    class EjemploImprenta: provando la aplicacion

--- src/org/Jhonatan/PooInterfaces/Imprenta/EjemploImprenta.java
+++ src/org/Jhonatan/PooInterfaces/Imprenta/EjemploImprenta.java
@@ -1,13 +1,17 @@
 package org.Jhonatan.PooInterfaces.Imprenta;
 
 import org.Jhonatan.PooInterfaces.Imprenta.Modelo.Curriculo;
+import org.Jhonatan.PooInterfaces.Imprenta.Modelo.Genero;
 import org.Jhonatan.PooInterfaces.Imprenta.Modelo.Hoja;
 import org.Jhonatan.PooInterfaces.Imprenta.Modelo.Informe;
+import org.Jhonatan.PooInterfaces.Imprenta.Modelo.Libro;
+import org.Jhonatan.PooInterfaces.Imprenta.Modelo.Pagina;
 
 public class EjemploImprenta {
     public static void main(String[] args) {
         System.out.println("Ejemplo De Implementacion de Interfaces");
-        Ejemplo();
+        // Ejemplo();
+        ejemploLibro();
     }
 
     public static void Ejemplo() {
@@ -23,6 +27,35 @@ public class EjemploImprenta {
         Informe informe = new Informe("Martin Fowler", "James", "Estudio sobre microservicios");
         imprimir(cv);
         imprimir(informe);
+    }
+
+    public static void ejemploLibro() {
+        Curriculo cv = new Curriculo("Jhon Doe", "Ingeniero de Sistemas", "Resumen Laboral");
+
+        // objeto de tipo libro
+        Libro libro = new Libro("Erich Gamma",
+                "Patrones de Diseño: Elem. Reusables POO",
+                Genero.PROGRAMACIÓN);
+        libro.addPagina(new Pagina("Patrón"));
+
+        // anadimos paginas
+        libro.addPagina(new Pagina("Patrón Singleton"))
+                .addPagina(new Pagina("Patrón Observador"))
+                .addPagina(new Pagina("Patrón Factory"))
+                .addPagina(new Pagina("Patrón Composite"))
+                .addPagina(new Pagina("Patrón Facade"));
+
+        // añadimos experiencias
+        cv.addExperecia("Java");
+        cv.addExperecia("Oracle DBA");
+        cv.addExperecia("Spring Framework");
+        cv.addExperecia("Desarrollador FullSack");
+        cv.addExperecia("Angular");
+
+        Informe informe = new Informe("Martin Fowler", "James", "Estudio sobre microservicios");
+        imprimir(cv);
+        imprimir(informe);
+        libro.imprimir();
     }
 
     public static void imprimir(Hoja imprimible) {
